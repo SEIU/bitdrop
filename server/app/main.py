9f402714f4807391cb3a449cc60f01ba9a944ffc
David Mertz, Ph.D. <david.mertz@seiu.org>
2025-10-27 13:46:35 -0400

    All routes complete, need tests

--- server/app/main.py
+++ server/app/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+import base64
+from datetime import datetime
+from pathlib import Path
+from typing import Annotated
+
+from fastapi import Body, FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -6,3 +12,67 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/api/upload/{id}/{raw_hash}/{filename}")
+async def upload_file(
+    id: str,
+    raw_hash: str,
+    filename: str,
+    file: Annotated[bytes, Body()],
+) -> JSONResponse:
+    "Store raw bytes of an uploaded file"
+    ts = datetime.now().isoformat(timespec="seconds")
+    fname = Path.home() / f"uploads" / ts / id / raw_hash / filename
+    fname.parent.mkdir(parents=True, exist_ok=True)
+    with open(fname, "wb") as f:
+        f.write(file)
+
+    return JSONResponse(content={"id": id, "filename": filename, "timestamp": ts})
+
+
+@app.get("/api/dowload/{id}")
+def download_file(id: str) -> JSONResponse:
+    "Download a file by its ID token"
+    uploads_dir = Path.home() / "uploads"
+
+    matches = list(uploads_dir.glob(f"**/{id}/**"))
+    if not matches:
+        return JSONResponse(
+            content={"message": f"No file found with ID {id}"}, status_code=404
+        )
+    elif len(matches) > 1:
+        return JSONResponse(
+            content={"message": f"Multiple files found with ID {id}"}, status_code=409
+        )
+    else:
+        file = matches[0]
+        _, _, raw_hash, filename = file.parts
+        base64_content = base64.b64encode(file.read_bytes())
+        return JSONResponse(
+            content={
+                "filename": filename,
+                "raw_hash": raw_hash,
+                "base64_content": base64_content,
+            }
+        )
+
+
+@app.delete("/api/download/{id}/{raw_hash}")
+def delete_file(id: str, raw_hash: str) -> JSONResponse:
+    "Delete a file by its ID and raw hash"
+    uploads_dir = Path.home() / "uploads"
+
+    matches = list(uploads_dir.glob(f"**/{id}/{raw_hash}/**"))
+    if not matches:
+        return JSONResponse(
+            content={"message": f"No file found with ID {id} and raw hash {raw_hash}"},
+            status_code=404,
+        )
+    else:
+        for file in matches:
+            file.unlink()
+
+        return JSONResponse(
+            content={"message": f"File with ID {id} and raw hash {raw_hash} deleted"}
+        )
