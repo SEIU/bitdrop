46482537e6aea83780a476734a19e9112cf65e48
Calico Seders <calico.seders@seiu.org>
2025-11-04 11:20:36 -0800

    set production url to http://api.b2.seiu.org/

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -6,7 +6,7 @@ export function getBackendUrl() {
   if (window.location.hostname === "localhost") {
     return developmentUrl;
   }
-  return "";
+  return "http://api.b2.seiu.org/";
 }
 
 export const generatePassword = async () => {
