afe0c9b865c1563cf1e51e8e8fef187c930778fc
Calico Seders <calico.seders@seiu.org>
2025-11-06 09:56:01 -0800

    update backend url

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -6,7 +6,7 @@ export function getBackendUrl() {
   if (window.location.hostname === "localhost") {
     return developmentUrl;
   }
-  return "https://localhost:8443";
+  return "https://127.0.0.1:8443";
 }
 
 export const generatePassword = async () => {
